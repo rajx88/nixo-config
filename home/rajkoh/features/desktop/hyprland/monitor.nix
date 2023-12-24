75ffe982ddf3f90b5d2ded2c3cd4d86f3079a081
rajx88 <rajx88@dotkohlen.dev>
2023-12-24 13:46:40 +0100

    split up into multiple files

---
+++ home/rajkoh/features/desktop/hyprland/monitor.nix
@@ -0,0 +1,14 @@
+{config, ...}: {
+  wayland.windowManager.hyprland.settings = {
+    monitor = map (
+      m: let
+        resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
+        position = "${toString m.x}x${toString m.y}";
+      in "${m.name},${
+        if m.enabled
+        then "${resolution},${position},1"
+        else "disable"
+      }"
+    ) (config.monitors);
+  };
+}
