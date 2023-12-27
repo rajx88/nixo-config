a8e042d482d3072f54bea6e5beef1bf44aec5896
rajx88 <rajx88@dotkohlen.dev>
2023-12-27 16:50:56 +0100

    Typo in steam config, changed the look and feel of alacritty and nvim

---
+++ home/rajkoh/features/desktop/_common/discord/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  home = {
+    packages = with pkgs; [
+      # discord
+      # (discord.override {
+      #   # has troubles showing bar with close/minimize buttons
+      #   # withOpenASAR = true;
+      #   # withVencord = true;
+      #   # nss = nss_latest;
+      # })
+
+      discord
+      # discocss
+    ];
+  };
+}
