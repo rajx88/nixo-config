117ac42862565f8781dd88e363402bc9a7933524
rajx88 <44810778+rajx88@users.noreply.github.com>
2023-12-29 21:24:43 +0100

    gtk theme and vscode plugins

--- home/rajkoh/features/desktop/_common/gtk.nix
+++ home/rajkoh/features/desktop/_common/gtk.nix
@@ -6,6 +6,14 @@
 }: rec {
   gtk = {
     enable = true;
+    # theme = {
+    #   name = "Sweet-mars";
+    #   package = pkgs.sweet;
+    # };
+    theme = {
+      name = "rose-pine";
+      package = pkgs.rose-pine-gtk-theme;
+    };
     font = {
       name = config.fontProfiles.regular.family;
       size = 12;
@@ -19,7 +27,7 @@
   services.xsettingsd = {
     enable = true;
     settings = {
-      # "Net/ThemeName" = "${gtk.theme.name}";
+      "Net/ThemeName" = "${gtk.theme.name}";
       "Net/IconThemeName" = "${gtk.iconTheme.name}";
     };
   };
