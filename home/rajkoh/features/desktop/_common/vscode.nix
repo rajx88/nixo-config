d12837859c4092ec3f628557d4d801f4b2a98171
rajx88 <rajx88@dotkohlen.dev>
2023-12-30 00:28:13 +0100

    added some basic vscode extensions

--- home/rajkoh/features/desktop/_common/vscode.nix
+++ home/rajkoh/features/desktop/_common/vscode.nix
@@ -4,23 +4,25 @@
     userSettings = {
       # needed for hyprland will crash imeediately without it
       "window.titleBarStyle" = "custom";
+      "workbench.colorTheme" = "Rosé Pine Moon";
     };
     # package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      github.copilot
-      github.copilot-chat
-      kamadorueda.alejandra
-      jnoortheen.nix-ide
-      mkhl.direnv
-    ];
-    # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
-    # [
-    #   {
-    #     name = "tokyo-night";
-    #     publisher = "Avetis";
-    #     version = "1.0.1";
-    #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-    #   }
-    # ];
+    extensions = with pkgs.vscode-extensions;
+      [
+        github.copilot
+        github.copilot-chat
+        kamadorueda.alejandra
+        jnoortheen.nix-ide
+        mkhl.direnv
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
+      [
+        {
+          name = "rose-pine";
+          publisher = "mvllow";
+          version = "2.9.0";
+          sha256 = "ibx19iDUXumpc1vTIUubceFyWyD7nUEBlunFDMcdW6E=";
+        }
+      ];
   };
 }
