64d8e22db2bc4603a0716d88fbfc4010462aa26d
rajx88 <rajx88@dotkohlen.dev>
2023-12-29 21:24:43 +0100

    gtk theme and vscode plugins

--- home/rajkoh/features/desktop/_common/vscode.nix
+++ home/rajkoh/features/desktop/_common/vscode.nix
@@ -6,7 +6,21 @@
       "window.titleBarStyle" = "custom";
     };
     # package = pkgs.vscodium;
-    # extensions = with pkgs.vscode-extensions; [
+    extensions = with pkgs.vscode-extensions; [
+      github.copilot
+      github.copilot-chat
+      kamadorueda.alejandra
+      jnoortheen.nix-ide
+      mkhl.direnv
+    ];
+    # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace
+    # [
+    #   {
+    #     name = "tokyo-night";
+    #     publisher = "Avetis";
+    #     version = "1.0.1";
+    #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+    #   }
     # ];
   };
 }
