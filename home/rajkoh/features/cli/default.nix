9716f5b24833785aa199fae885b6e3276d4bb99d
rajx88 <rajx88@dotkohlen.dev>
2024-01-02 23:20:17 +0100

    added script to change wallpaper to random

--- home/rajkoh/features/cli/default.nix
+++ home/rajkoh/features/cli/default.nix
@@ -39,6 +39,8 @@
 
     lazygit
 
+    killall
+
     zip
     unzip
   ];
--- home/rajkoh/features/desktop/hyprland/swaybg.nix
+++ home/rajkoh/features/desktop/hyprland/swaybg.nix
@@ -9,7 +9,19 @@ in {
     swaybg
   ];
 
+  xdg.configFile."hypr" = {
+    recursive = true;
+    source = ./scripts;
+    target = "hypr/scripts";
+  };
+
   wayland.windowManager.hyprland.settings = {
+    bind = let
+      script = "${pkgs.python3}/bin/python3 ${config.xdg.configHome}/hypr/scripts/wall-changer.py";
+    in [
+      "$mod SHIFT,r,exec, ${script} ${wallpaper}"
+    ];
+
     exec = [
       "${pkgs.swaybg}/bin/swaybg -i ${wallpaper}/wall-01.jpg --mode fill"
     ];
