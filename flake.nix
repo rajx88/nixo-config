eb5c87967fa207f855bdbfb359ac65d41757b9e6
rajx88 <rajx88@dotkohlen.dev>
2024-01-26 10:50:19 +0100

    Added new arch installation, added 2 generic profiles for non-nixos systems

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
-          ./home/non-nixos/arch.nix
+          ./home/rickoh/generic.nix
         ];
       };
     };
