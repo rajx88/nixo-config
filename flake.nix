a1a715f421d3d4d9bc055099d2e4e6d7f2c5476a
rajx88 <rajx88@dotkohlen.dev>
2024-01-26 22:51:23 +0100

    switcherooni

--- flake.nix
+++ flake.nix
@@ -5,14 +5,14 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    impermanence.url = "github:nix-community/impermanence";
+    hardware.url = "github:nixos/nixos-hardware";
+
     # Home manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    impermanence.url = "github:nix-community/impermanence";
-    hardware.url = "github:nixos/nixos-hardware";
 
     disko = {
       url = "github:nix-community/disko";
--- hosts/_common/optional/1password.nix
+++ hosts/_common/optional/1password.nix
@@ -13,9 +13,7 @@
   # this needs to be here to integrate with the browser plugin
   # todo make this conditional for different hosts???
   programs = {
-    _1password = {
-      enable = true;
-    };
+    _1password.enable = true;
     _1password-gui = {
       enable = true;
       polkitPolicyOwners = ["rajkoh"];
