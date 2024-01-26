dcdd7f276cf18320f05432414147fe3c2d340483
rajx88 <rajx88@dotkohlen.dev>
2024-01-26 18:19:18 +0100

    added asdf for arch machine

--- home/rajkoh/features/cli/asdf.nix
+++ home/rajkoh/features/cli/asdf.nix
@@ -3,25 +3,15 @@
   config,
   ...
 }: {
-  # home.file.".tool-versions".text = ''
-  #   kubectl 1.28.4
-  # '';
-
   home.packages = with pkgs; [
     asdf-vm
   ];
 
   programs.zsh = {
-    enable = true;
     initExtra = ''
-      [[ ! -f "${config.xdg.dataHome}/asdf/plugins/java/set-java-home.zsh" ]] || . "${config.xdg.dataHome}/asdf/plugins/java/set-java-home.zsh"
+      export ASDF_DATA_DIR="${config.xdg.configHome}/asdf"
+
+      [[ ! -f "$ASDF_DATA_DIR/plugins/java/set-java-home.zsh" ]] || . "$ASDF_DATA_DIR/plugins/java/set-java-home.zsh"
     '';
-
-    # initExtraBeforeCompInit = ''
-    #   [[ ! -f "${config.xdg.dataHome}/asdf/asdf.sh" ]] || . "${config.xdg.dataHome}/asdf/asdf.sh"
-
-    #   # append completions to fpath
-    #   fpath=($fpath ${config.xdg.dataHome}/asdf/completions)
-    # '';
   };
 }
