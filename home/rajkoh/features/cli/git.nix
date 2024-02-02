bf6e121d27208297a6afae079d96cd6c1f796811
rajx88 <rajx88@dotkohlen.dev>
2024-02-02 15:33:17 +0100

    git conditionals

--- home/rajkoh/features/cli/git.nix
+++ home/rajkoh/features/cli/git.nix
@@ -2,11 +2,19 @@
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
-    userName = "rajkohlen";
-    userEmail = "rajkohlen@gmail.com";
     extraConfig = {
       init.defaultBranch = "main";
     };
     lfs.enable = true;
+    includes = [
+      {
+        condition = "gitdir:~/code/work/";
+        path = "${config.xdg.configHome}/git/work.inc";
+      }
+      {
+        condition = "gitdir:~/code/prvt/";
+        path = "${config.xdg.configHome}/git/prvt.inc";
+      }
+    ];
   };
 }
