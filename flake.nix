43a77af617161383310a264e1f10c31a9a7147b9
rajx88 <44810778+rajx88@users.noreply.github.com>
2024-05-07 18:20:55 +0200

    Keeping 5 revisions instead of days and added cachix for hyprland

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "nixos configuration";
 
+  nixConfig = {
+    substituters = [
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
