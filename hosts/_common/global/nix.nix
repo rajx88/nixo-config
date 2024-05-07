9a40f18b7d24f0f56a5909dedb9db2c12fcbb246
rajx88 <rajx88@dotkohlen.dev>
2024-05-07 18:20:55 +0200

    Keeping 5 revisions instead of days and added cachix for hyprland

--- hosts/_common/global/nix.nix
+++ hosts/_common/global/nix.nix
@@ -5,9 +5,16 @@
 }: {
   nix = {
     settings = {
-      trusted-users = ["root" "@wheel"];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
       auto-optimise-store = lib.mkDefault true;
-      experimental-features = ["nix-command" "flakes" "repl-flake"];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "repl-flake"
+      ];
       warn-dirty = false;
       # Disable global flake registry
       flake-registry = "";
@@ -15,16 +22,12 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      # Keep the last 7 days generations
-      options = "--delete-older-than 7d";
+      # Keep the last 3 generations
+      options = "--delete-older-than +5";
     };
 
-    # Add each flake input as a registry
-    # To make nix3 commands consistent with the flake
-    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
-
-    # Add nixpkgs input to NIX_PATH
-    # This lets nix2 commands still use <nixpkgs>
-    nixPath = ["nixpkgs=${inputs.nixpkgs.outPath}"];
+    # Add each flake input as a registry and nix_path
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 }
