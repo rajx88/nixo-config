0becd34222e3e47942fc7e3e8e855d9051f53d3d
rajx88 <rajx88@dotkohlen.dev>
2024-05-24 00:09:23 +0200

    wip: new nvim config from scratch

---
+++ home/rajkoh/features/cli/nvim/config/lua/custom/plugins/which-keys.lua
@@ -0,0 +1,6 @@
+return {
+  -- Fuzzy Finder (files, lsp, etc)
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+}
---
+++ home/rajkoh/features/cli/nvim/config/plugin/options.lua
@@ -0,0 +1,51 @@
+local opt = vim.opt
+
+-- set termguicolors to enable highlight groups
+opt.termguicolors = true
+
+-- disable background
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+opt.inccommand = "split"
+opt.splitbelow = true
+opt.splitright = true
+
+-- See `:help vim.o`
+-- Set highlight on search
+opt.hlsearch = false
+
+-- Make line numbers default
+opt.number = true
+opt.relativenumber = true
+
+-- Enable mouse mode
+--vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+--opt.breakindent = true
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+
+-- Decrease update time
+opt.updatetime = 50
+
+-- Set completeopt to have a better completion experience
+--vim.o.completeopt = 'menuone,noselect'
+
+
