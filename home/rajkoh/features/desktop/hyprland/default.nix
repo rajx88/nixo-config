a11b7c90388a7ac590aa3a8af94d4305e18d3954
rajx88 <rajx88@dotkohlen.dev>
2024-08-11 12:55:43 +0200

    back to master setup

--- home/rajkoh/features/desktop/hyprland/default.nix
+++ home/rajkoh/features/desktop/hyprland/default.nix
@@ -49,15 +49,15 @@
         gaps_in = 8;
         gaps_out = 8;
         border_size = 2;
-        layout = "dwindle";
+        layout = "master";
       };
 
-      # dwindle = {
-      #   # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-      #   pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-      #   preserve_split = true; # you probably want this
-      #   split_width_multiplier = 1.35;
-      # };
+      dwindle = {
+        # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+        pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = true; # you probably want this
+        split_width_multiplier = 1.35;
+      };
 
       master = {
         # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
