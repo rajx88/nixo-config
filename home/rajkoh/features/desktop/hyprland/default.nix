72111d237c3f13aa0fd806594d65d07047e149a6
rajx88 <44810778+rajx88@users.noreply.github.com>
2024-08-11 11:41:46 +0200

    removed hyprland option deprecated

--- home/rajkoh/features/desktop/hyprland/default.nix
+++ home/rajkoh/features/desktop/hyprland/default.nix
@@ -52,16 +52,16 @@
         layout = "dwindle";
       };
 
-      dwindle = {
-        # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-        pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-        preserve_split = true; # you probably want this
-        split_width_multiplier = 1.35;
-      };
+      # dwindle = {
+      #   # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+      #   pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+      #   preserve_split = true; # you probably want this
+      #   split_width_multiplier = 1.35;
+      # };
 
       master = {
         # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-        new_is_master = true;
+        new_status = "master";
       };
 
       input = {
