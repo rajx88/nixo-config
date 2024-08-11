55da8f2ac59d63c814babadf621e0f06438ff8e7
rajx88 <44810778+rajx88@users.noreply.github.com>
2024-08-11 11:55:21 +0200

    added a few mini plugins
    Added plugins for mini.nvim:
    
    - comment
    - diff
    - indentscope
    
    Furthermore added docs for the respectable plugins.

--- home/rajkoh/features/cli/nvim/config/lua/custom/plugins/mini.lua
+++ home/rajkoh/features/cli/nvim/config/lua/custom/plugins/mini.lua
@@ -1,7 +1,19 @@
 return {
   "echasnovski/mini.nvim",
+  version = false, -- this the default and recommended
+  -- version = "*", -- this is the stable branch
   config = function()
+    -- select magic: https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-ai.md
     require("mini.ai").setup()
+    -- surround text, https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-surround.md
     require("mini.surround").setup()
+    -- commenting magic
+    require("mini.comment").setup()
+    -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-operators.md
+    require("mini.operators").setup()
+    -- shows what lines has changed
+    require("mini.diff").setup()
+    -- indicates the indenting scope
+    require("mini.indentscope").setup()
   end,
 }
