bd67440d14a99f87cd81cc57eecd3433cf8ca900
rajx88 <rajx88@dotkohlen.dev>
2025-01-13 18:01:00 +0100

    updated themes

--- home/rajkoh/features/dev/mise.nix
+++ home/rajkoh/features/dev/mise.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: {
-  programs.mise = {
-    enable = true;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
+  programs = {
+    mise = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+    };
+    zsh.initExtra = ''
+      eval "$(${pkgs.mise}/bin/mise activate zsh --shims)"
+    '';
   };
 }
