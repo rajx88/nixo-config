5614e94c6215828194f97b3313100fe2def68100
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-01-14 09:58:05 +0100

    bind keys for standard navigation and delete

--- home/rajkoh/features/cli/zsh.nix
+++ home/rajkoh/features/cli/zsh.nix
@@ -51,27 +51,48 @@ in {
       setopt glob_complete
 
       if [[ -d $ZSH_PINCLUDES ]]; then
-          for file in $ZSH_PINCLUDES/*.zsh(DN); do
-              if [[ -f $file && -r $file ]]; then
-                  # Source the file
-                  source $file
-              fi
-          done
+      	for file in $ZSH_PINCLUDES/*.zsh(DN); do
+      		if [[ -f $file && -r $file ]]; then
+        	# Source the file
+        	source $file
+      		fi
+      	done
       fi
 
     '';
 
     initExtra = ''
+
+      bindkey -M emacs "^[[3~" delete-char
+      bindkey -M viins "^[[3~" delete-char
+      bindkey -M vicmd "^[[3~" delete-char
+
+      # [Ctrl-Delete] - delete whole forward-word
+      bindkey -M emacs '^[[3;5~' kill-word
+      bindkey -M viins '^[[3;5~' kill-word
+      bindkey -M vicmd '^[[3;5~' kill-word
+
+      # [Ctrl-RightArrow] - move forward one word
+      bindkey -M emacs '^[[1;5C' forward-word
+      bindkey -M viins '^[[1;5C' forward-word
+      bindkey -M vicmd '^[[1;5C' forward-word
+
+      # [Ctrl-LeftArrow] - move backward one word
+      bindkey -M emacs '^[[1;5D' backward-word
+      bindkey -M viins '^[[1;5D' backward-word
+      bindkey -M vicmd '^[[1;5D' backward-word
+
+
       typeset -U path PATH
       path=($HOME/.local/bin $path)
 
       if [[ -d $ZSH_INCLUDES ]]; then
-          for file in $ZSH_INCLUDES/*.zsh(DN); do
-              if [[ -f $file && -r $file ]]; then
-                  # Source the file
-                  source $file
-              fi
-          done
+      for file in $ZSH_INCLUDES/*.zsh(DN); do
+      	if [[ -f $file && -r $file ]]; then
+      		# Source the file
+      		source $file
+      	fi
+      done
       fi
     '';
 
