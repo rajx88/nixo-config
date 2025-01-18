3c0365e44200408dbb34f6005ccbdd1be93ea9fe
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-01-18 14:57:07 +0100

    fix monitors

--- home/rajkoh/features/desktop/hyprland/config/monitor.nix
+++ home/rajkoh/features/desktop/hyprland/config/monitor.nix
@@ -1,14 +1,38 @@
 {config, ...}: {
   wayland.windowManager.hyprland.settings = {
-    monitor = map (
-      m: let
-        resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
-        position = "${toString m.x}x${toString m.y}";
-      in "${m.name},${
-        if m.enabled
-        then "${resolution},${position},1"
-        else "disable"
-      }"
-    ) (config.monitors);
+    monitor = let
+      waybarSpace = let
+        inherit (config.wayland.windowManager.hyprland.settings.general) gaps_in gaps_out;
+        inherit (config.programs.waybar.settings.primary) position height width;
+        gap = gaps_out - gaps_in;
+      in {
+        top =
+          if (position == "top")
+          then height + gap
+          else 0;
+        bottom =
+          if (position == "bottom")
+          then height + gap
+          else 0;
+        left =
+          if (position == "left")
+          then width + gap
+          else 0;
+        right =
+          if (position == "right")
+          then width + gap
+          else 0;
+      };
+    in
+      [
+        ",addreserved,${toString waybarSpace.top},${toString waybarSpace.bottom},${toString waybarSpace.left},${toString waybarSpace.right}"
+      ]
+      ++ (map (
+        m: "${m.name},${
+          if m.enabled
+          then "${toString m.width}x${toString m.height}@${toString m.refreshRate},${m.position},1"
+          else "disable"
+        }"
+      ) (config.monitors));
   };
 }
