db07012b282b542133097fd08db7f51a2dbdc135
rajx88 <rajx88@dotkohlen.dev>
2025-03-02 00:33:27 +0100

    remember tuigreet

--- hosts/_common/optional/greetd.nix
+++ hosts/_common/optional/greetd.nix
@@ -26,7 +26,7 @@ in {
     enable = true;
     settings.default_session = {
       user = "rajkoh";
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
     };
   };
 
