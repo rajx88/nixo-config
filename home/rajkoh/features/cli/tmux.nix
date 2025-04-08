8438084a28bfa637c273921e04202eabd7c436ea
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-04-08 19:00:30 +0200

    tmux

--- home/rajkoh/features/cli/tmux.nix
+++ home/rajkoh/features/cli/tmux.nix
@@ -15,51 +15,31 @@
     keyMode = "vi";
     mouse = true;
 
-    # plugins = with pkgs; [
-    # set some default keybindings, e.g. prefix + r for reload
-    # for more information go to https://github.com/tmux-plugins/tmux-sensible
-    # tmuxPlugins.sensible
-    # resurrect and continuum
-    # tmuxPlugins.resurrect
-    # {
-    #   plugin = tmuxPlugins.continuum;
-    #   extraConfig = ''
-    #     set -g @continuum-restore 'on'
-    #     set -g @continuum-save-interval '60' # minutes
-    #   '';
-    # }
-    # tmuxPlugins.vim-tmux-navigator
-    # tmuxPlugins.open
-
-    # styling
-    # {
-    #   plugin = tmuxPlugins.catppuccin;
-    #   extraConfig = ''
-    #     set -g @catppuccin_flavour 'mocha'
-    #     set -g @catppuccin_pill_theme_enabled on
-    #   '';
-    # }
-    # ];
-
     extraConfig = ''
 
-      #########################################
-      ####        general tmux options     ####
-      #########################################
       set -ga terminal-overrides ",screen-256color*:Tc"
       set-option -g default-terminal "screen-256color"
+      set -s escape-time 0
 
+      unbind C-b
+      set-option -g prefix C-a
+      bind-key C-a send-prefix
       set -g status-style 'bg=#333333 fg=#5eacd3'
 
-      # Allow tmux to set the terminal title
-      set -g set-titles on
+      bind r source-file ~/.config/tmux/tmux.conf \; display-message "Config reloaded"
+      set -g base-index 1
 
-      # re-number windows when one is closed
-      set -g renumber-windows on
+      set-window-option -g mode-keys vi
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
 
-      ############################
-      ####        binds       ####
-      ############################
+      # vim-like pane switching
+      bind -r ^ last-window
+      bind -r k select-pane -U
+      bind -r j select-pane -D
+      bind -r h select-pane -L
+      bind -r l select-pane -R
+
       # Split windows
       unbind %
       unbind '"'
@@ -72,24 +52,6 @@
       # Easier and faster switching between next/prev window
       bind C-p previous-window
       bind C-n next-window
-
-      # Source .tmux.conf as suggested in `man tmux`
-      bind R source-file '~/.config/tmux/tmux.conf'
-
-      ###################################
-      ####        vim keybinds       ####
-      ###################################
-
-      set-window-option -g mode-keys vi
-      bind -r ^ last-window
-      bind -r k select-pane -U
-      bind -r j select-pane -D
-      bind -r h select-pane -L
-      bind -r l select-pane -R
-
-      bind -T copy-mode-vi v send-keys -X begin-selection
-      bind -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
 
     '';
   };
