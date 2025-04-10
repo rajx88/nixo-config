4aa9d05a97b035d9d7f5729d4391a744d6aae244
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-04-10 16:35:18 +0200

    new colorscheme

--- home/rajkoh/features/cli/nvim/config/lua/custom/plugins/colorschemes.lua
+++ home/rajkoh/features/cli/nvim/config/lua/custom/plugins/colorschemes.lua
@@ -41,7 +41,28 @@ return {
         transparent = true,
       }
 
-      vim.cmd [[ colorscheme fluoromachine ]]
+      -- vim.cmd [[ colorscheme fluoromachine ]]
+    end,
+  },
+  {
+    "lunarvim/synthwave84.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("synthwave84").setup {
+        glow = {
+          error_msg = true,
+          type2 = true,
+          func = true,
+          keyword = true,
+          operator = false,
+          buffer_current_target = true,
+          buffer_visible_target = true,
+          buffer_inactive_target = true,
+        },
+      }
+
+      -- vim.cmd [[ colorscheme synthwave84 ]]
     end,
   },
   {
@@ -50,7 +71,18 @@ return {
     priority = 1000,
     config = function(plugin)
       vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      -- vim.cmd [[ colorscheme aura-dark ]]
+      vim.cmd [[ colorscheme aura-dark ]]
+      -- vim.cmd [[ colorscheme aura-dark-soft-text ]]
+      -- vim.cmd [[ colorscheme aura-soft-dark ]]
+      -- vim.cmd [[ colorscheme aura-soft-dark-soft-text ]]
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd [[ colorscheme kanagawa]]
     end,
   },
 }
