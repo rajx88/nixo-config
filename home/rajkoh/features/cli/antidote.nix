34ce1aad128f2192aef4159af898b4b5c03c2406
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-04-13 20:59:49 +0200

    antidote update

--- home/rajkoh/features/cli/antidote.nix
+++ home/rajkoh/features/cli/antidote.nix
@@ -1,15 +1,26 @@
-{
-  programs.zsh.antidote = {
-    enable = true;
-    plugins = [
-      # "romkatv/zsh-defer"
-      "mattmc3/ez-compinit"
-      "zsh-users/zsh-completions"
-      "aloxaf/fzf-tab"
-      "zdharma-continuum/fast-syntax-highlighting"
-      "zsh-users/zsh-autosuggestions"
-      "zsh-users/zsh-history-substring-search"
-      # "spaceship-prompt/spaceship-prompt"
+{config, ...}: {
+  home.persistence = {
+    "/persist/home/rajkoh".directories = [
+      ".local/share/antidote"
     ];
+  };
+
+  programs.zsh = {
+    sessionVariables = {
+      ANTIDOTE_HOME = "${config.xdg.dataHome}/antidote";
+    };
+    antidote = {
+      enable = true;
+      plugins = [
+        # "romkatv/zsh-defer"
+        "mattmc3/ez-compinit"
+        "zsh-users/zsh-completions"
+        "aloxaf/fzf-tab"
+        "zdharma-continuum/fast-syntax-highlighting"
+        "zsh-users/zsh-autosuggestions"
+        "zsh-users/zsh-history-substring-search"
+        # "spaceship-prompt/spaceship-prompt"
+      ];
+    };
   };
 }
