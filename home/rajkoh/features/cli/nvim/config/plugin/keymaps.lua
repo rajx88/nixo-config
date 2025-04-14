9191000bd638d1c7434a938335ff4d5b808f57cd
rajx88 <rajx88@dotkohlen.dev>
2025-04-14 13:16:55 +0200

    added keymaps for nvim

--- home/rajkoh/features/cli/nvim/config/plugin/keymaps.lua
+++ home/rajkoh/features/cli/nvim/config/plugin/keymaps.lua
@@ -1,5 +1,33 @@
 local set = vim.keymap.set
 
+-- file explorer
+set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Visual mode Move selected text up and down
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Joins lines without moving the cursor
+set("n", "J", "mzJ`z")
+
+-- Scroll up and down and center the cursor
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor centered when cycling through the search results
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
+
+-- clipboard pasting
+set("x", "<leader>p", [["_dP]])
+
+-- yank to system clipboard
+set({ "n", "v" }, "<leader>y", [["+y]])
+set("n", "<leader>Y", [["+Y]])
+
+-- delete to the blackhole register
+set({ "n", "v" }, "<leader>d", '"_d')
+
 -- Basic movement keybinds, these make navigating splits easy for me
 set("n", "<c-j>", "<c-w><c-j>")
 set("n", "<c-k>", "<c-w><c-k>")
