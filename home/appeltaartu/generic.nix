1674b807994cf110d098bcd89f9be1bc7350fb1c
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-05-19 21:34:48 +0200

    wsl alacritty and mise

--- home/appeltaartu/generic.nix
+++ home/appeltaartu/generic.nix
@@ -1,8 +1,15 @@
 {lib, ...}: {
   imports = [
     ../rajkoh/generic.nix
+    ../rajkoh/features/dev/mise.nix
   ];
 
-  home.username = lib.mkForce "appeltaartu";
   programs.ssh.enable = lib.mkForce false;
+
+  home = {
+    username = lib.mkForce "appeltaartu";
+    sessionVariables = {
+      TERM = lib.mkForce "alacritty";
+    };
+  };
 }
