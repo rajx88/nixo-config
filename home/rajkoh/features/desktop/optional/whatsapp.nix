054ba8fb8eee0b2a08beac07896f9ab0670119e6
rajx88 <rajx88@dotkohlen.dev>
2025-06-19 16:35:22 +0200

    Removed whatsapp as it wasn't working, and added signal, persist atuin

--- home/rajkoh/features/desktop/optional/whatsapp.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    whatsapp-for-linux
-  ];
-
-  home.persistence = {
-    "/persist/home/rajkoh".directories = [".local/share/whatsapp-for-linux"];
-  };
-}
