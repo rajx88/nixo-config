037d4ee1dfabd150f3a5046b68943337fccf13a6
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-06-19 16:35:22 +0200

    Removed whatsapp as it wasn't working, and added signal, persist atuin

--- home/rajkoh/akarnae.nix
+++ home/rajkoh/akarnae.nix
@@ -10,6 +10,7 @@
 
     ./features/desktop/optional/discord
     ./features/desktop/optional/spotify.nix
+    ./features/desktop/optional/signal.nix
 
     # not working for some reason
     # ./features/desktop/optional/obsidian.nix
