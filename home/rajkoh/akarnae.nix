63a21b491981755729f87bc96ea0de6dd661af30
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-11-02 10:50:23 +0100

    new display setup

--- home/rajkoh/akarnae.nix
+++ home/rajkoh/akarnae.nix
@@ -39,45 +39,23 @@
   monitors = [
     {
       name = "DP-1";
-      width = 2560;
-      height = 1440;
-      refreshRate = 240;
+      width = 3840;
+      height = 2160;
+      refreshRate = 120;
       workspace = "2";
-      position = "0x0";
-      primary = true;
+      position = "auto-right";
     }
-    # vertical setup
-    # {
-    #   name = "HDMI-A-1";
-    #   width = 1440;
-    #   height = 2560;
-    #   refreshRate = 144;
-    #   position = "auto-left";
-    #   workspace = "1";
-    #   vertical = "1";
-    # }
-    {
-      name = "HDMI-A-1";
-      width = 2560;
-      height = 1440;
-      # width = 1440;
-      # height = 2560;
-      refreshRate = 60;
-      position = "auto-left";
-      # vertical = "1";
-      workspace = "1";
-    }
-
     {
       name = "DP-2";
-      width = 2560;
-      height = 1440;
+      width = 3840;
+      height = 2160;
       # width = 1440;
       # height = 2560;
-      refreshRate = 144;
-      position = "auto-right";
-      vertical = "3";
-      workspace = "3";
+      refreshRate = 120;
+      position = "auto";
+      # vertical = "3";
+      workspace = "1";
+      primary = true;
     }
   ];
 }
