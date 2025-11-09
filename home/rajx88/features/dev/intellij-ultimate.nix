dc7bc158c5487c7d742afe29e296f86afb1365af
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-11-09 21:55:59 +0100

    refactor: restore JetBrains IDE settings persistence configuration

--- home/rajx88/features/dev/intellij-ultimate.nix
+++ home/rajx88/features/dev/intellij-ultimate.nix
@@ -1,7 +1,11 @@
 {pkgs, ...}: {
-  # home.persistence = {
-  #   "/persist/home/rajx88".directories = [".local/share/JetBrains"];
-  # };
+  # Persist JetBrains IDE settings and metadata across reboots with impermanence.
+  home.persistence = {
+    "/persist/home/rajx88".directories = [
+      ".local/share/JetBrains"
+      ".config/JetBrains"
+    ];
+  };
 
   home.packages = with pkgs; [
     jetbrains.idea-ultimate
