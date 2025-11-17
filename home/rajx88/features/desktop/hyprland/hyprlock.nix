866015130006c15d5cf1975c3d80041d3f1ccb62
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-11-17 09:13:40 +0100

    fix: increase timeout values for session lock and DPMS commands

--- home/rajx88/features/desktop/hyprland/hyprlock.nix
+++ home/rajx88/features/desktop/hyprland/hyprlock.nix
@@ -101,11 +101,11 @@
           on-timeout = "brightnessctl set 50%-";
         }
         {
-          timeout = 120;
+          timeout = 300;
           on-timeout = "loginctl lock-session";
         }
         {
-          timeout = 140;
+          timeout = 320;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
