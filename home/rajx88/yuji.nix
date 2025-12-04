e115c7ecfb921d37268449e8b9bf6ba7e31a3182
rajx88 <44810778+rajx88@users.noreply.github.com>
2025-12-04 14:48:40 +0100

    add tidal

--- home/rajx88/yuji.nix
+++ home/rajx88/yuji.nix
@@ -12,6 +12,7 @@
 
     ./features/desktop/optional/discord
     ./features/desktop/optional/spotify.nix
+    ./features/desktop/optional/tidal.nix
     ./features/desktop/optional/signal.nix
     ./features/desktop/optional/ferdium.nix
     ./features/desktop/optional/teams.nix
