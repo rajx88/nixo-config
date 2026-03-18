29955816adef0455757c300af18dbad85de3e1ce
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-03-18 08:33:50 +0100

    bluetooth settings

--- hosts/_common/optional/bluetooth.nix
+++ hosts/_common/optional/bluetooth.nix
@@ -4,6 +4,13 @@
   ...
 }: {
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings = {
+    Policy.AutoEnable = "true";
+    General = {
+      FastConnectable = "true";
+      Experimental = "true";
+    };
+  };
   services.blueman.enable = true;
   environment.systemPackages = with pkgs; [blueman bluez];
   environment.persistence."/persist".directories = ["/var/lib/bluetooth"];
