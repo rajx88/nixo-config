a4df1b9721787583bbd4f72a1a169338758f9c03
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-03-19 16:02:56 +0100

    clean up starship config, remove unused toml and settings

--- home/rajx88/features/cli/starship/default.nix
+++ home/rajx88/features/cli/starship/default.nix
@@ -1,33 +1,8 @@
 {
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
-  # xdg.configFile."starship" = {
-  #   source = ./config;
-  #   recursive = true;
-  # };
-
-  # home.sessionVariables.STARSHIP_CONFIG = "${config.xdg.configHome}/starship/starship.toml";
-
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
     enableFishIntegration = true;
-    settings = {
-      git_status = {
-        ignore_submodules = true;
-        windows_starship = "";
-      };
-      git_branch = {
-        ignore_bare_repo = true;
-        format = "on [$symbol$branch(:$remote_branch)]($style) ";
-      };
-      directory = {
-        truncate_to_repo = false;
-      };
-    };
   };
 }
