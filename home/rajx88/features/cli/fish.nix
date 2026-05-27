e7acb2a46f3afb4a726bf9a6b2aac5436f2c94ad
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-05-27 16:07:47 +0200

    Update fish and prmt Aura theme colors
    Adjust syntax highlighting colors in fish shell config and change git
    branch color from orange (#ffca85) to green (#61ffca) in prmt prompts
    for both zsh and fish.

--- home/rajx88/features/cli/fish.nix
+++ home/rajx88/features/cli/fish.nix
@@ -54,16 +54,16 @@ in {
     };
 
     interactiveShellInit = ''
-      # Aura theme colors (universal so they persist)
+      # Aura theme colors for fish syntax highlighting
       if not set -q __fish_aura_applied
         set -U fish_color_normal edecee
-        set -U fish_color_command 61ffca
+        set -U fish_color_command a277ff
         set -U fish_color_keyword f694ff
-        set -U fish_color_quote ffca85
+        set -U fish_color_quote 61ffca
         set -U fish_color_redirection f694ff
-        set -U fish_color_end 61ffca
+        set -U fish_color_end a277ff
         set -U fish_color_error ff6767
-        set -U fish_color_param a277ff
+        set -U fish_color_param 82e2ff
         set -U fish_color_comment 6d6d6d
         set -U fish_color_operator a277ff
         set -U fish_color_escape f694ff
--- home/rajx88/features/cli/prmt.nix
+++ home/rajx88/features/cli/prmt.nix
@@ -29,7 +29,7 @@
       typeset -ga precmd_functions
       precmd_functions+=(_prmt_git_sync)
 
-      PROMPT='$(prmt --shell zsh --code $? " {path:#a277ff} {git:#ffca85:f: } {env:#ff6767:PRMT_GIT_SYNC} {time:#82e2ff:24hs} \n{ok:#61ffca}{fail:#ff6767} ")'
+      PROMPT='$(prmt --shell zsh --code $? " {path:#a277ff} {git:#61ffca:f: } {env:#ff6767:PRMT_GIT_SYNC} {time:#82e2ff:24hs} \n{ok:#61ffca}{fail:#ff6767} ")'
     '';
   };
 
@@ -59,7 +59,7 @@
       body = ''
         set -l last_status $status
         _prmt_git_sync
-        prmt --shell none --code $last_status "{path:#a277ff} {git:#ffca85:f: } {env:#ff6767:PRMT_GIT_SYNC} {time:#82e2ff:24hs} {ok:#61ffca:>}{fail:#ff6767:>} "
+        prmt --shell none --code $last_status "{path:#a277ff} {git:#61ffca:f: } {env:#ff6767:PRMT_GIT_SYNC} {time:#82e2ff:24hs} {ok:#61ffca:>}{fail:#ff6767:>} "
       '';
     };
   };
