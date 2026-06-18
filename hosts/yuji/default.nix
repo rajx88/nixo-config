099efc12834e1583afdc6b2b2cbde1a8cb58a8c0
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-06-19 00:32:44 +0200

    Disable hindsight for yuji host
    Comment out hindsight module import to disable the memory backend.

--- hosts/yuji/default.nix
+++ hosts/yuji/default.nix
@@ -29,7 +29,7 @@
     ../_common/optional/openvpn.nix
     ../_common/optional/net-debug.nix
     ../_common/optional/wireguard.nix
-    ../_common/optional/hindsight.nix
+    # ../_common/optional/hindsight.nix
   ];
 
   nixpkgs.config.nvidia.acceptLicense = true;
