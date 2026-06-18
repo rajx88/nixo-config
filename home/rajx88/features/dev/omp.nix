93abac7f2fe05153dfe07e95372faf63d6a79b91
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-06-18 18:08:24 +0200

    Add omp fish shell completions
    Initialize omp completions in the fish interactive shell to enable command completion support for the omp command.

--- home/rajx88/features/dev/omp.nix
+++ home/rajx88/features/dev/omp.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   home.packages = [pkgs.omp];
 
+  programs.fish.interactiveShellInit = ''
+    omp completions fish | source
+  '';
+
   home.persistence."/persist".directories = [
     ".omp"
   ];
