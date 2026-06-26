465437b4021b396cb1abe0fb8b0cc0bad7fd7ced
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-06-26 10:30:09 +0200

    Update omp to 16.1.22

--- pkgs/omp/default.nix
+++ pkgs/omp/default.nix
@@ -9,11 +9,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "omp";
-  version = "16.1.19";
+  version = "16.1.22";
 
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-nL7lP9OAespK6StzUbYN5KG3agWHbU3Y59vClBbaSt8=";
+    hash = "sha256-HifmJkmjjIeHdYfFIhc94A6wFqXjjbCk8/nQYv22+cM=";
   };
 
   nativeBuildInputs = [
