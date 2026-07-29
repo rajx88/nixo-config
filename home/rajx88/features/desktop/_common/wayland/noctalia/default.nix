69bcfe4141cde6789389d0fb49c4691505331f86
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-07-29 22:15:21 +0200

    fix: correct noctalia panel placement values and bar config structure

--- home/rajx88/features/desktop/_common/wayland/noctalia/default.nix
+++ home/rajx88/features/desktop/_common/wayland/noctalia/default.nix
@@ -12,9 +12,12 @@ in {
         font_family = "Atkinson Hyperlegible Mono";
         panel = {
           control_center_placement = "floating";
-          launcher_placement = "centered";
-          session_placement = "centered";
-          wallpaper_placement = "centered";
+          launcher_placement = "floating";
+          session_placement = "floating";
+          wallpaper_placement = "floating";
+          launcher_position = "center";
+          session_position = "center";
+          wallpaper_position = "center";
           open_near_click_control_center = true;
           open_near_click_clipboard = true;
           open_near_click_session = true;
@@ -47,30 +50,25 @@ in {
         battery.display_mode = "graphic";
       };
 
-      bar = {
-        order = ["default"];
-        default = {
-          position = "top";
-          background_opacity = 0.69;
-          capsule = false;
-          # margin_ends = 10;
-          # margin_edge = 10;
+      bar.main = {
+        position = "top";
+        background_opacity = 0.69;
+        capsule = false;
 
-          start = ["launcher" "cpu" "ram" "temp" "wallpaper" "media"];
-          center = ["workspaces" "clipboard"];
-          end = [
-            "tray"
-            "notifications"
-            "network"
-            "bluetooth"
-            "volume"
-            "brightness"
-            "battery"
-            "clock"
-            "control-center"
-            "session"
-          ];
-        };
+        start = ["launcher" "cpu" "ram" "temp" "wallpaper" "media"];
+        center = ["workspaces" "clipboard"];
+        end = [
+          "tray"
+          "notifications"
+          "network"
+          "bluetooth"
+          "volume"
+          "brightness"
+          "battery"
+          "clock"
+          "control-center"
+          "session"
+        ];
       };
     };
   };
