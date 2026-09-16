36edcf07f978bd351c09b0357c77bd4c78827173
rajx88 <44810778+rajx88@users.noreply.github.com>
2026-09-16 10:28:24 +0200

    chore(pkgs/omp): bump version from 18.2.0 to 18.2.1
    This commit updates the omp package to version 18.2.1 and updates the corresponding sha256 hash to match the new release.

--- pkgs/omp/default.nix
+++ pkgs/omp/default.nix
@@ -9,11 +9,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "omp";
-  version = "18.2.0";
+  version = "18.2.1";
 
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    hash = "sha256-QbZ6Q/GKfNM8wN13Kk+gQs0gq2xX5r1FH/bb5Vp5eNg=";
+    hash = "sha256-nHZIXE5lh1Z4uIx5JsfmiR1Ih5ZnbzWd6cTfWEyIb8k=";
   };
 
   nativeBuildInputs = [
